92246a739e5a371f861a58493dca18c1830ec373
McGregorSt <stasiak.gregor@gmail.com>
2019-09-11 12:58:38 +0200

    Updates

---
+++ precache-manifest.ef8c0976d21f65494f4c1b1da5f30114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399a4d7ade53fb88dbe76f69ccb1f15b",
+    "url": "/burger_builder/index.html"
+  },
+  {
+    "revision": "e3fabd1f2211bcd68514",
+    "url": "/burger_builder/static/css/main.f42ab32d.chunk.css"
+  },
+  {
+    "revision": "0df8eb8d5d51a5de6b1c",
+    "url": "/burger_builder/static/js/2.f1c1879c.chunk.js"
+  },
+  {
+    "revision": "e3fabd1f2211bcd68514",
+    "url": "/burger_builder/static/js/main.532d9d6e.chunk.js"
+  },
+  {
+    "revision": "e879623255057b6a7c22",
+    "url": "/burger_builder/static/js/runtime~main.0a450c14.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
